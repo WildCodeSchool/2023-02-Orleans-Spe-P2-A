b074d4ccfda0d5823d9c464d5eea55eb36b75329
Ga-Mikaël <tchiki.tshiki@gmail.com>
2023-03-17 14:42:56 +0100

    Added some texts and image to the start-page.

--- src/js/Start.js
+++ src/js/Start.js
@@ -6,9 +6,27 @@ export default class Start extends Phaser.Scene {
 
     preload() {
         this.load.audio('bgm', './src/assets/music-start.mp3');
+        this.load.image('bg-start', './src/assets/bg-start1.png');
     }
 
     create() {
+        this.add.image(1000, 500, 'bg-start').setScale(.9);
+
+        this.add.text(500, 100, "CRAZYGAME", {
+                fontSize: "100px",
+                fill: "#000000"
+            });
+
+        this.add.text(450, 200, "Un pong ? un shooter ? Des chats ?", {
+            fontSize: "35px",
+            fill: "#000000"
+        });
+
+        this.add.text(700, 400, "Appuie sur espace pour commencer", {
+            fontSize: "30px",
+            fill: "#000000"
+        });
+
         this.bgm = this.sound.add('bgm');
         this.bgmConfig = {
             volume: 0.8,
